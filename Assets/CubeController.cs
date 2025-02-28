8845bfb9661391b412837fe1807f7c8d590b7485
dabqu <dabqu@dabqu.dabqu>
2025-02-28 10:56:00 -0600

    initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class CubeController : MonoBehaviour
+{
+    public int foo = 10;
+    public int bar = 10;
+    public GameObject cubePrefab;
+
+    void Awake()
+    {
+        Debug.Log("Awoken");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+    void Update()
+    {
+        Debug.Log("Update");    
+    }
+
+    void FixedUpdate()
+    {
+        Debug.Log("FixedUpdate");
+    }
+
+    void OnDestroy()
+    {
+        Debug.Log("OnDestroy");
+    }
+    
+}
